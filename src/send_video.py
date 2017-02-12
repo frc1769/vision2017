15b8e6bcce19f8fe45def7d7e924f5c600d91c25
John Blessing <jcbless@gmail.com>
2017-02-12 02:10:06 +0000

    Adjusting exposure and adding network tables code

--- src/send_video.py
+++ src/send_video.py
@@ -23,8 +23,17 @@ import sys
 import subprocess
 import threading
 import Queue
+from networktables import NetworkTables
+
+HEIGHT = 360
+WIDTH = 640
 
 running = True
+#NetworkTables.initialize(server = "10.17.69.69")
+sd = NetworkTables.getTable("CameraData")
+
+subprocess.call(['v4l2-ctl','-c', 'exposure_auto=1'])
+subprocess.call(['v4l2-ctl','-c', 'exposure_absolute=5'])
 
 def video_proc():
         while running:
@@ -37,7 +46,7 @@ video = v4l2capture.Video_device("/dev/video0")
 
 # Suggest an image size to the device. The device may choose and
 # return another size if it doesn't support the suggested one.
-size_x, size_y = video.set_format(640, 360)
+size_x, size_y = video.set_format(WIDTH, HEIGHT)
 
 # Create a buffer to store image data in. This must be done before
 # calling 'start' if v4l2capture is compiled with libv4l2. Otherwise
@@ -76,7 +85,36 @@ try:
 	res = gp.process(im_array)
 
         if not video_queue.full():
-	    video_queue.put(res)
+	    video_queue.put(res[0])
+	    
+        points = res[1]
+        print points
+        print "]"
+        x_sum = 0
+        y_sum = 0
+        size_sum = 0
+        ct_pts = 0
+        for p in points:
+            ct_pts += 1
+            x_sum += p.pt[0]
+            y_sum += p.pt[1]
+            size_sum += p.size
+        if ct_pts:    
+            x_val = x_sum / ct_pts / WIDTH * 2 - 1
+            y_val = y_sum / ct_pts / HEIGHT * 2 - 1
+            size_val = size_sum / ct_pts / WIDTH
+            print x_val, y_val, size_val, ct_pts
+        else:
+            x_val = 0
+            y_val = 0
+            size_val = 0
+            
+        sd.putNumber('x', x_val)
+        sd.putNumber('y', y_val)
+        sd.putNumber('size', size_val)
+        sd.putNumber('count', ct_pts)
+            
+        print "]"
 	
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
